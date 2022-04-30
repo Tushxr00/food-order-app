c2fdc7abf448bbddc96a7c96c7a1ea008dbdc4a1
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-30 18:38:15 +0530

    made a dummy list of data

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,5 +1,15 @@
+import React from "react";
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
 import classes from "./Meals.module.css";
 
-const Meals = (props) => {};
+const Meals = () => {
+  return (
+    <React.Fragment>
+      <AvailableMeals />
+      <MealsSummary />
+    </React.Fragment>
+  );
+};
 
 export default Meals;
